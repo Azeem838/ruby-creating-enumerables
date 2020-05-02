c1a0b6218ca257c14d09b861470182d96d9a36a8
Azeem <azeemahmed101@gmail.com>
2020-05-02 18:47:35 +0200

    add select method

--- enumerables.rb
+++ enumerables.rb
@@ -1,23 +1,33 @@
-arr = [5, 2, 3]
+arr = [5, 2, 3, 8, 10, 14, 16]
 
 module Enumerable
-  def my_each(array)
+  def my_each()
     i = -1
-    array.length.times do
+    length.times do
       i += 1
-      yield array[i]
+      yield self[i]
     end
   end
 
-  def my_each_with_index(array)
+  def my_each_with_index()
     i = -1
-    array.length.times do
+    length.times do
       i += 1
-      yield array[i], i
+      yield self[i], i
     end
+  end
+
+  def my_select(&block)
+    new_array = []
+    my_each do |item|
+      new_array << item if block.call(item) == true
+    end
+    new_array
   end
 end
+# arr.my_each { |x| puts x }
+# arr.my_each_with_index { |idx, x| puts "#{idx}, #{x}" }
 
-arr.my_each_with_index(arr) { |x, ind| puts "#{x}, #{ind}" }
+p arr.my_select(&:even?)
 
-arr.each_with_index { |y, idx| puts "#{y}, #{idx}" }
+p arr.select(&:even?)
