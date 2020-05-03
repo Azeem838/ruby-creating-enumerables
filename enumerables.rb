cf35723f85a61383827b7dcc866aa681d3df74c6
Azeem <azeemahmed101@gmail.com>
2020-05-03 16:05:35 +0200

    add my_all method

--- enumerables.rb
+++ enumerables.rb
@@ -24,10 +24,24 @@ module Enumerable
     end
     new_array
   end
+
+  def my_all?(&block)
+    new_array = []
+    my_each do |item|
+      new_array << (block.call(item) == true ? 1 : 0)
+    end
+    count = 0
+    new_array.my_each do |x|
+      count += x
+    end
+    count == new_array.length
+  end
 end
 # arr.my_each { |x| puts x }
 # arr.my_each_with_index { |idx, x| puts "#{idx}, #{x}" }
 
-p arr.my_select(&:even?)
+# p arr.my_select(&:even?)
+# p arr.select(&:even?)
 
-p arr.select(&:even?)
+puts(arr.my_all? { |x| x > 1 })
+puts(arr.all? { |x| x > 1 })
